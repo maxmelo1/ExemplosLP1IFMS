2364f08562914ed5011543680167e295fb67a68f
maxmelo1 <>
2020-04-17 17:21:43 -0400

    ajuste da nomenclatura dos pacotes.

--- src/br/edu/ifms/tratamentoExcecoes/ContaBancaria.java
+++ src/br/edu/ifms/tratamentoExcecoes/ContaBancaria.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package exemploslp1ifms;
+package br.edu.ifms.tratamentoExcecoes;
 
 import javax.swing.JOptionPane;
 
