45fc7be87d7208aa11ddc3050969f055f839dfa5
maxmelo1 <>
2020-04-17 16:57:21 -0400

    versão para os exercicios em aula.

--- src/exemploslp1ifms/ContaBancaria.java
+++ src/exemploslp1ifms/ContaBancaria.java
@@ -5,17 +5,56 @@
  */
 package exemploslp1ifms;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author gin
  */
 public class ContaBancaria {
 
+    
+    Double saldo = 0.0;
+    
+    void verSaldo(){
+            JOptionPane.showMessageDialog( null, "Saldo atual = R$ " + saldo);
+
+    }
+
+    void depositar(Double quantia){
+            saldo += quantia;
+    }
+
+    void sacar(Double quantia){
+        saldo -= quantia;
+    }
+
+    public ContaBancaria() {
+        //teste aqui o programa!!!!!!!!!!!!
+
+        //lembre-se, o sufixo d indica casting(coerção) para double. 
+        //Você sempre pode associar um double a um Double, mas o contrário não é verdade. Por quê?
+        
+        
+        /*******************************
+        * use para o exercício no final
+        * (double) ( (int)(Math.random()*(300)) );
+        * 
+        * Random gerador = new Random();
+        * gerador.nextInt(300);
+        * 
+        *******************************/
+        
+    }
+    
+    
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        new ContaBancaria();
     }
     
 }
